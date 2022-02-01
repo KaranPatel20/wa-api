6919678084c56b571a1557dd73a9b3b89292635c
Karan Patel <karankp20120@gmail.com>
2022-02-01 12:32:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import SearchParams from "./SearchParams";
+import React from "react";
+import { render } from "react-dom";
+  const App = () => {
+    return React.createElement("div", {}, [
+      <SearchParams/>
+      
+    ]);
+  };
+  
+  //ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
+/*        const res = fetch(
+            `https://api.whatsapp.com/send?phone=91${finalNumber}&text=${finalMessage}`,
+            {method: 'GET', mode: 'no-cors', redirect: 'follow', headers: {
+                'Content-Type': 'url/api'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                },}
+        );*/
